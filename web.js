acfd2b89b509da3abe206744f09714ada13e2da0
Olivier Catteau <ocatteau@gmail.com>
2013-07-14 16:34:02 +0000

    readFile

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World !!');
+  //var hello = fs.readFileSync('index.html').toString('utf-8');
+  response.send('hello');
 });
 
 var port = process.env.PORT || 5000;
